f67732a0c80d4d919d2a105eefc0e3379fcfd5ca
oxana-ermolenko <oxana.ermolenko.dev@gmail.com>
2020-10-02 10:42:05 -0700

    Updates

---
+++ precache-manifest.6ca3bc168c9ac41ee8a27a3e7b19c686.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f9bd42986bd025cbaed10109c4d52f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2cc393182d3727976cc0",
+    "url": "/portfolio/static/css/main.1e58579b.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc393182d3727976cc0",
+    "url": "/portfolio/static/js/main.c9eedc8a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "98f0bf434e9c2c931b89232deb283991",
+    "url": "/portfolio/static/media/c-d-x-5qT09yIbROk-unsplash.98f0bf43.jpg"
+  },
+  {
+    "revision": "44a396cab15a58886e2cdae86dc61185",
+    "url": "/portfolio/static/media/header-bg2.44a396ca.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/portfolio/static/media/map-image.f40ed5b1.png"
+  }
+]);
